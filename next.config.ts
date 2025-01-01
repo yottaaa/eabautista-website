f8ff3b8166f81962e6d0984f2a59ac74ae8f2a9a
Erick A. Bautista <yotterick7@gmail.com>
2025-01-02 01:11:50 +0800

    fix errors

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'github.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'royeen.vercel.app',
-      },
-    ],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
